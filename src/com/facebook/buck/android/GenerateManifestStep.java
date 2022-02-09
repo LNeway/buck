54295432d276fa55f21ba04fc23471c1687b9cb6
liuwei11 <liuwei11@kuaishou.com>
2022-02-09 19:27:37 +0800

    添加merge 的文件路径，帮助定位问题

--- src/com/facebook/buck/android/GenerateManifestStep.java
+++ src/com/facebook/buck/android/GenerateManifestStep.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import sun.rmi.runtime.Log;
 
 public class GenerateManifestStep implements Step {
 
